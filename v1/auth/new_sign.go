3520a5325874db3226345039256abc008bd6754b
JERRY <hwjameshw@gmail.com>
2023-06-14 16:17:47 +0900

    refactor: 소스 정리

--- v1/auth/new_sign.go
+++ v1/auth/new_sign.go
@@ -14,28 +14,18 @@ import (
 // @Summary 토큰 재발급
 // @Description 토큰 재발급
 // @Security AppAuth
-// @Success 201 {object} model.DataResponse{data=auth.NewSign.data} "success"
+// @Success 201 {object} model.DefaultResponse "success"
 // @Router /v1/auth/sign/new [post]
 func NewSign(c *gin.Context) {
-	type data struct {
-		AccessToken string `json:"accessToken" binding:"required"`
-	}
-
 	user := c.MustGet("user").(model.User)
-
-	isSecure := true
-	if os.Getenv("APP_MODE") == "local" {
-		isSecure = false
-	}
 
 	accessToken := util.GetAccessToken(&user)
 
+	isSecure := os.Getenv("APP_MODE") != "local"
+
 	c.SetCookie("access-token", accessToken, 60*30, "/", os.Getenv("APP_HOST"), isSecure, true)
 
-	c.JSON(http.StatusCreated, &model.DataResponse{
+	c.JSON(http.StatusCreated, &model.DefaultResponse{
 		Message: "success",
-		Data: &data{
-			AccessToken: accessToken,
-		},
 	})
 }
